1d309e2a1d23c5bc9c273882e985742d2da2da0d
Mabry Cervin <mpcervin@uncg.edu>
2015-04-04 18:26:48 -0400

    Changed Service to a form that works with hdeamonize

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
 import Prelude hiding (FilePath)
-import System.Posix.Daemon
+import System.Posix.Daemonize hiding (name)
 import Shelly
 import qualified Data.Text as T
 import Data.Default
@@ -10,20 +10,17 @@ import Data.Either.Utils
 import Control.Monad.Error
 
 data Service = Service { name :: T.Text
-                       , start :: FilePath
-                       , stop :: FilePath
+                       , exec :: FilePath
                        } deriving Show
 
 buildServices :: MonadError C.CPError m => C.ConfigParser -> [C.SectionSpec] -> [m Service]
 buildServices cp = fmap (buildService cp)
 
-
 buildService :: MonadError C.CPError m => C.ConfigParser -> C.SectionSpec -> m Service
 buildService cp section = do
         name <- C.get cp section "name"
-        start <- C.get cp section "start"
-        stop <- C.get cp section "stop" 
-        return $ Service {name=name, start=fromText start, stop=fromText stop}
+        exec <- C.get cp section "exec"
+        return $ Service {name=name, exec=fromText exec}
 
 main = shelly $ do
     val <- liftIO $ C.readfile C.emptyCP "test.cfg"

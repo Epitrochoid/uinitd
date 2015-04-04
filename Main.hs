bb6c139e4aab0e9c68d07d20622e92bf05d5550a
Mabry Cervin <mpcervin@uncg.edu>
2015-04-04 17:38:38 -0400

    Added Service datatype and Service builder

--- Main.hs
+++ Main.hs
@@ -1,10 +1,32 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
+import Prelude hiding (FilePath)
 import System.Posix.Daemon
 import Shelly
 import qualified Data.Text as T
 import Data.Default
+import qualified Data.ConfigFile as C
+import Data.Either.Utils
+import Control.Monad.Error
+
+data Service = Service { name :: T.Text
+                       , start :: FilePath
+                       , stop :: FilePath
+                       } deriving Show
+
+-- buildServices :: [C.SectionSpec] -> C.ConfigParser -> [Either Service]
 
 
-main = runDetached (Just "test.pid") def $ shelly $ do
-    sleep 10
+buildService :: MonadError C.CPError m => C.SectionSpec -> C.ConfigParser -> m Service
+buildService section cp = do
+        name <- C.get cp section "name"
+        start <- C.get cp section "start"
+        stop <- C.get cp section "stop" 
+        return $ Service {name=name, start=fromText start, stop=fromText stop}
+
+main = shelly $ do
+    val <- liftIO $ C.readfile C.emptyCP "test.cfg"
+    let cp = forceEither val
+
+    let a = buildService "section1" cp
+    echo $ T.pack $ show $ forceEither a

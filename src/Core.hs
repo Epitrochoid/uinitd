b10ad610ae54c0dc385b99e04adb4928831cf8a4
Mabry Cervin <mpcervin@uncg.edu>
2015-04-11 19:56:57 -0400

    Added runUinitd and defaults

--- src/Core.hs
+++ src/Core.hs
@@ -6,6 +6,24 @@ import Types
 import Control.Monad.State
 import Control.Monad.Reader
 
+runUinitd :: Config -> UinitdState -> Uinitd a -> IO (a, UinitdState)
+runUinitd conf state (Uinitd a) = runStateT (runReaderT a conf) state
+
+defConfig :: Config
+defConfig = Config {
+          serviceDir = "",
+          execDir = "",
+          serviceList = "",
+          logFile = "",
+          pidDir = ""
+}
+
+defUinitdSt :: UinitdState
+defUinitdSt = UinitdState {
+            available = [],
+            running = []
+}
+
 addService :: Service -> Uinitd ()
 addService service = do
         UinitdState{..} <- get
